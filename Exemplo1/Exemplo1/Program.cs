86f6612d6a7f69e953ca0ab76af972e7ee7b3ec0
Filipe Maciel <filipemaciellopes01@gmail.com>
2023-08-14 17:50:19 -0400

    Ultima aula

---
+++ Exemplo1/Exemplo1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] valores = new int[10];
+            double[] preco = new double[10];
+            string[] nomepro = new string[10];
+
+  
+            for (int i = 0; i <= valores.Length; i++)
+            {
+                Console.WriteLine("Digite um Valor: ");
+                valores[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i <= valores.Length; i++) {
+                Console.WriteLine("Valor: "+ valores[i]);
+            }
+
+            Console.ReadKey();
+
+           
+        }
+    }
+}
